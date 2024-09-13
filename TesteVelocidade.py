9edb609070dcfb3d82a5bea73d2d308605a539d9
niteoliveira <142188575+niteoliveira@users.noreply.github.com>
2024-09-13 09:00:42 -0300

    Update TesteVelocidade.py

--- TesteVelocidade.py
+++ TesteVelocidade.py
@@ -1,5 +1,5 @@
 import time
-
+#1)
 problemSize = 5000
 print("%12s%16s" %("Problem Size","Seconds"))
 for count in range(5):
@@ -11,3 +11,19 @@ for count in range(5):
     elapsed = time.time() - start
     print("%12d%16.5f" %(problemSize,elapsed))
     problemSize *= 4
+#2)
+
+
+problemSize = 1000
+print("%12s%16s" %("Problem Size","Interations"))
+for count in range(5):
+    number = 0
+    work = 1
+    for x in range(problemSize):
+        for k in range(problemSize):
+            number +=1 
+            work += 1
+            work -= 1
+    
+    print("%12d%16.5f" %(problemSize,number))
+    problemSize *= 2
